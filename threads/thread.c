43c224610c8d6adc4fb1053f305ab2c1f291103b
cocomodo <programmingonly17@gmail.com>
2023-06-23 03:20:03 +0000

    Project 1: Alam Clock - thread.c-thread_sleep 내부 변경

--- threads/thread.c
+++ threads/thread.c
@@ -320,7 +320,8 @@ thread_sleep(int64_t ticks){ // ticks: 깨어야 할 시각
 	ASSERT(curr != idle_thread); // 현재 스레드가 idle이 아닐 때만
 	curr->wakeup_ticks = ticks; // 일어날 시각 지정
 
-	list_insert_ordered(&sleep_list, &curr->elem, less_ticks, NULL);//sleep_list에 curr_sleep을 추가
+	// list_insert_ordered(&sleep_list, &curr->elem, less_ticks, NULL);//sleep_list에 curr_sleep을 추가
+	list_push_back(&sleep_list, &curr->elem);
 	thread_block(); // 현재 스레드 재우고 ready_list의 스레드 실행
 	
 	intr_set_level(old_level); // 인터럽트 상태를 원래 상태로 변경
@@ -329,8 +330,7 @@ thread_sleep(int64_t ticks){ // ticks: 깨어야 할 시각
 
 void
 thread_wakeup(int64_t global_ticks){
-	struct list_elem *curr_elem;
-	curr_elem=list_begin(&sleep_list);//sleep_list의 첫번째 요소 가져옴(빈 경우 NULL 반환)
+	struct list_elem *curr_elem = list_begin(&sleep_list);//sleep_list의 첫번째 요소 가져옴(빈 경우 NULL 반환)
 
 
 	enum intr_level old_level;
@@ -341,13 +341,12 @@ thread_wakeup(int64_t global_ticks){
 		if(global_ticks >= curr_thread->wakeup_ticks){//깰 시간이 됐으면
 			curr_elem=list_remove(curr_elem); //sleep_list에서 제거
 			thread_unblock(curr_thread); //ready_list로 이동
-			break;
 		}
 		else{
 			curr_elem=list_next(curr_elem);
 		}
+		intr_set_level(old_level); //인터럽트 상태를 원래 상태로 변경
 	}
-	intr_set_level(old_level); //인터럽트 상태를 원래 상태로 변경
 
 	// //깨울 스레드 찾기
 	// struct sleep_thread_elem *root_thread = list_entry(list_head(&sleep_list), struct sleep_thread_elem, elem);
